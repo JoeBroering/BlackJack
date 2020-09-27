8f9dfb1bb7c316de8d62c36810b687c1f22562db
JoeBroering <joebroering17@gmail.com>
2020-09-27 17:26:27 -0400

    starter files

---
+++ src/csc439team7/blackjack.java
@@ -0,0 +1,9 @@
+package csc439team7;
+
+/**
+ * @version 1.0
+ * @author joebr
+ */
+
+public class blackjack {
+}
---
+++ tests/csc439team7/blackjackTest.java
@@ -0,0 +1,12 @@
+package csc439team7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @version 1.0
+ * @author joebr
+ */
+
+class blackjackTest {
+
+}
