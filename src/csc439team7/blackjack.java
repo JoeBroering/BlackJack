34455c83658708d9308111622e486d34fde3f93d
jcody <codyj2@nku.edu>
2020-09-29 23:58:17 -0400

    In progress card and card test

--- src/csc439team7/blackjack.java
+++ src/csc439team7/blackjack.java
@@ -7,4 +7,75 @@ package csc439team7;
  */
 
 public class blackjack {
+
+    /**
+     * Class used to create a card object
+     * @author Jackson Cody
+     * @version 1.0
+     */
+    class card {
+
+        /**
+         * Number and suit variables for a card
+         */
+        private int number;
+        private int suit;
+        private String numberName;
+
+        /**
+         * @param suit suit of the card
+         * @param numb number of the card
+         */
+        public card (int suit, int numb){
+            this.number=numb;
+            this.suit=suit;
+        }
+
+        /**
+         * @return number of the card object
+         */
+        public int getNumber() {
+            return number;
+        }
+
+        /**
+         * sets the number of the card
+         * @param number number of the card
+         */
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        /**
+         * returns the name of the card if it is a face card
+         * @param numb number of the card object
+         */
+        public String getCardName(int numb){
+            return " ";
+        }
+
+        /**
+         * @return suit of the card object
+         */
+        public int getSuit() {
+            return suit;
+        }
+
+        /**
+         * @param suit suit of the card
+         */
+        public void setSuit(int suit) {
+            this.suit = suit;
+        }
+
+        /**
+         * returns the name of the suit of the card
+         * @param suit suit of the card
+         */
+        public String getSuitName(int suit){
+            return " ";
+        }
+
+
+    }
 }
---
+++ tests/csc439team7/cardTest.java
@@ -0,0 +1,38 @@
+package csc439team7;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class cardTest {
+
+    @Test
+    public void getNumberTest() {
+
+    }
+
+    @Test
+    public void setNumber() {
+
+    }
+
+    @Test
+    public void getSuittest() {
+
+    }
+
+    @Test
+    public void setSuit() {
+
+    }
+
+    @Test
+    public void getSuitName() {
+
+    }
+
+    @Test
+    public void getNumberName() {
+
+    }
+}
