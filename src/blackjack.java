0380ef47a6b1cbfeac985a0c7719cf0107cd27be
JoeBroering <joebroering17@gmail.com>
2020-09-30 10:52:58 -0400

    added more testing cases

--- src/blackjack.java
+++ src/blackjack.java
@@ -1,44 +1,45 @@
-
-/**
- * testing 5:53
- * @version 1.0
- * @author joebr
- */
-
-
     /**
      * Class used to create a card object
      * @author Jackson Cody
-     * @version 1.0
+     * @version 1.1
      */
      class card {
 
-        /**
-         * Number and suit variables for a card
-         */
         private int number;
         private int suit;
-        private String numberName;
 
         /**
+         * constructor for card that takes in a suit and a number and checks for illegal arguments
+         * @version 1.1
          * @param suit suit of the card
-         * @param numb number of the card
+         * @param number number of the card
          */
-        public card (int suit, int numb){
-            this.number=numb;
+        public card (int suit, int number){
+            if (number < 1 || number > 14) {
+                throw new IllegalArgumentException();
+            }
+            if (suit < 0 || suit > 3) {
+                throw new IllegalArgumentException();
+            }
+            this.number=number;
             this.suit=suit;
         }
 
         /**
+         * gets the Number of the card, stays at 11,12,13,14 for face cards. does not need to handle illegal arguments because other methods handle them before ever reaching this one.
          * @return number of the card object
+         * @version 1.0
+         * @author Jackson Cody
          */
         public int getNumber() {
             return number;
         }
 
         /**
-         * sets the number of the card
+         * @version 1.1
+         * sets the number of the card, checking for proper numbering first if not throws IllegalArgument
          * @param number number of the card
+         * @author joebr
          */
         public void setNumber(int number) {
             if (number < 1 || number > 14) {
@@ -49,43 +50,53 @@
 
         /**
          * returns the name of the card if it is a face card
-         * @param numb number of the card object
+         * @param number number of the card object
+         * @version 1.1
          */
-        public String getCardName(int numb){
-            if (numb < 1 || numb > 14){
+        public String getNumberName(int number){
+            if (number < 1 || number > 14){
                 throw new IllegalArgumentException();
             }
 
-            if (numb == 11){
+            if (number == 11){
                 return "jack";
-            } else if (numb == 12){
+            } else if (number == 12){
                 return "queen";
-            } else if(numb == 13){
+            } else if(number == 13){
                 return "king";
-            } else if(numb == 14){
+            } else if(number == 14 || number == 1){
                 return "ace";
             } else {
-                return Integer.toString(numb);
+                return Integer.toString(number);
             }
         }
 
         /**
+         * gets the int value of the suit, 0 hearts 1 spades 2 clubs 3 diamonds. does not check for illegal arguments because other methods handle them before reaching this one.
          * @return suit of the card object
+         * @version 1.0
          */
         public int getSuit() {
             return suit;
         }
 
         /**
+         * setSuit method which changes the suit of the card object, checks for a valid suit first otherwise throws illegal argument
+         * @author joebr
          * @param suit suit of the card
          */
+
         public void setSuit(int suit) {
+            if (suit < 0 || suit > 3) {
+                throw new IllegalArgumentException();
+            }
             this.suit = suit;
         }
 
         /**
-         * returns the name of the suit of the card
+         * returns the name of the suit of the card, and throws an illegal argument if the number isn't between 0 and 3 for a valid suit
          * @param suit suit of the card
+         * @author joebr
          */
         public String getSuitName(int suit){
             if(suit == 0) {
--- tests/cardTest.java
+++ tests/cardTest.java
@@ -6,10 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class cardTest {
 
     @Test
-    public void getNumberTest() {
+    public void getNumber() {
         card myCard = new card(0, 10);
         assertEquals(10, myCard.getNumber());
-
     }
 
     @Test
@@ -19,31 +18,88 @@ public class cardTest {
         assertEquals(10, mycard.getNumber());
     }
 
-    @Test
-    public void getSuittest() {
-        card mycard = new card(1,5);
-        assertEquals(1, mycard.getSuit());
+    @Test (expected = IllegalArgumentException.class)
+    public void setBadNumber() {
+        card mycard = new card(3,5);
+        mycard.setNumber(15);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void setBadNumber2() {
+        card mycard = new card(3,5);
+        mycard.setNumber(0);
     }
 
     @Test
-    public void setSuit() {
-
-    }
-
-    @Test
-    public void getSuitName() {
-    card mycard = new card(0,3);
-    assertEquals("hearts", mycard.getSuitName(0));
-    }
-
-
-    @Test
-    public void getCardName() {
+    public void getNumberName() {
         card mycard = new card(3,11);
-        assertEquals("jack", mycard.getCardName(mycard.getNumber()));
+        assertEquals("jack", mycard.getNumberName(mycard.getNumber()));
+    }
+
+    @Test
+    public void getNumberName2() {
+        card mycard = new card(3,8);
+        assertEquals("8", mycard.getNumberName(mycard.getNumber()));
+    }
+
+    @Test
+    public void getNumberName3() {
+        card mycard = new card(3,1);
+        assertEquals("ace", mycard.getNumberName(mycard.getNumber()));
+
+        card mycard2 = new card(3,14);
+        assertEquals("ace", mycard.getNumberName(mycard.getNumber()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void getBadNumberName() {
+        card mycard = new card(3,15);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void getBadNumberName2() {
+        card mycard = new card(3,0);
     }
 
     @Test
     public void getSuit() {
+        card mycard = new card(3,3);
+        assertEquals(3,mycard.getSuit());
     }
+
+    @Test
+    public void setSuit() {
+        card mycard = new card(2,11);
+        mycard.setSuit(3);
+        assertEquals(3, mycard.getSuit());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void setBadSuit() {
+        card mycard = new card(2,11);
+        mycard.setSuit(4);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void setBadSuit2() {
+        card mycard = new card(2,11);
+        mycard.setSuit(-1);
+    }
+
+    @Test
+    public void getSuitName() {
+        card mycard = new card(1,8);
+        assertEquals("spades",mycard.getSuitName(mycard.getSuit()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void getBadSuitName() {
+        card mycard = new card(-1,3);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void getBadSuitName2() {
+        card mycard = new card(4,3);
+    }
+
 }
